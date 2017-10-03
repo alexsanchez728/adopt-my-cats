9719f6d565d58b0df98daa5d6bc27c3f2a803137
Alex Sanchez <alesanchez728@gmail.com>
2017-10-03 14:03:48 -0500

    less than 10 toes are no shows

--- javascripts/events.js
+++ javascripts/events.js
@@ -9,12 +9,19 @@ $("#input").focus();
 $("#input").keyup((event) => {
 	if (event.key === "Enter") {
 		sendNumberOfCatz();
+		hideInput();
   }
 });
 
 $("#inputBtn").click(() => {
 	sendNumberOfCatz();
+	hideInput();
 });
+
+$("#disabled").click(() => {
+	hideSadCatz();
+});
+
 
 const sendNumberOfCatz = () => {
 	// get the input number request and save that array of cat data	
@@ -22,7 +29,16 @@ const sendNumberOfCatz = () => {
   cats.numberOfCatz($("#input").val());
 };
 	// hide the input and search buttons
+const hideInput = () => {
+	$("#input").addClass("hidden");
+	$("#inputBtn").addClass("hidden");
+	$("#disabled").removeClass("hidden");
 	// replace them with one "Get Rid Of The Disabeld Ones"
 		// any card that has a p tag with the class disabled-kitty should be hidden
+};
 
-		module.exports = {};
+const hideSadCatz = () => {
+	$(".disabled-cat").parent().parent().addClass("hidden");
+};
+
+module.exports = {};
